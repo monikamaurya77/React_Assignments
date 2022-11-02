69d8140adec46113dc117c63c0f4f981322822f2
Monika Maurya <96534851+monikamaurya77@users.noreply.github.com>
2022-11-02 18:23:40 +0530

    Add files via upload

---
+++ src/components/Main.js
@@ -0,0 +1,26 @@
+const Main = _ =>{
+    return(
+        <div>
+            <div className="Nav">
+<h1>About Us</h1>
+            </div>
+            <div className="sub-nav">
+            <p>At zappos.com, our purpose is simple: to live and deliver WOW.</p>
+              
+            </div>
+            <div className="main-content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                </p>
+            </div>
+            <div className="sub-main-content">
+            <p>We hope that in future people won't even realize we started sellingh shoes online. Instead,they'll know Zappos as a
+                 service company that just happens to sell______.</p>
+            </div>
+           
+        </div>
+    )
+}
+
+export default Main;
